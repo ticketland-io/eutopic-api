ba87d6fe88c75b14fdc1a35c8cb4b96277db76f1
ppoliani <ppoliani@gmail.com>
2022-07-21 15:04:39 +0300

    feat(api): create the new account

--- src/api/src/endpoints/account/create_account.rs
+++ src/api/src/endpoints/account/create_account.rs
@@ -1,21 +1,36 @@
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize};
-use serde_json;
+use data::{
+  repositories::account::upsert_account,
+};
 use crate::{
   utils::store::Store,
-  services::api_helpers::unauthorized_error,
+  services::{
+    api_helpers::unauthorized_error,
+    data::exec_basic_db_write_endpoint,
+  },
 };
 
 #[derive(Deserialize)]
 pub struct Body {
   firebase_token_id: String,
+  mnemonic: String,
 }
 
 pub async fn exec(store: web::Data<Store>, body: web::Json<Body>) -> HttpResponse {
   match store.firebase_auth.get_user_info(&body.firebase_token_id).await {
     Ok(user) => {
-      // 
-      HttpResponse::Ok().body(serde_json::to_string(&user).expect("unexpected user data received from firebase"))
+      exec_basic_db_write_endpoint(
+        store,
+        Box::new(move || {
+          upsert_account(
+            user.local_id.clone(),
+            body.mnemonic.clone(),
+          )
+        })
+      ).await;
+
+      HttpResponse::Created().finish()
     },
     Err(error) => {
       println!("{:?}", error);
