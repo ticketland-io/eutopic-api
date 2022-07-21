7bcdabfca6240fa1460aec5ae0e91fbabbaa04f9
ppoliani <ppoliani@gmail.com>
2022-07-21 18:25:22 +0300

    feat(data): add account model and update the create_account endpoint

--- src/api/src/endpoints/account/create_account.rs
+++ src/api/src/endpoints/account/create_account.rs
@@ -1,12 +1,18 @@
+use std::sync::Arc;
 use actix_web::{web, HttpResponse};
 use serde::{Deserialize};
 use data::{
-  repositories::account::upsert_account,
+  helpers::{send_read},
+  models::account::Account,
+  repositories::account::{
+    read_account,
+    upsert_account
+  },
 };
 use crate::{
   utils::store::Store,
   services::{
-    data::exec_basic_db_write_endpoint,
+    data::{exec_basic_db_write_endpoint},
   },
   middlewares::auth::AuthData,
 };
@@ -21,6 +27,22 @@ pub async fn exec(
   body: web::Json<Body>,
   auth: AuthData,
 ) -> HttpResponse {
+  let (query, db_query_params) = read_account(auth.user.local_id.clone());
+
+  let mnemonic = send_read(Arc::clone(&store.neo4j), query, db_query_params)
+  .await
+  .map(|db_result| {
+    let account: Account = db_result.try_into().unwrap();
+
+    return account.mnemonic;
+  });
+
+  // if account exist then simply return the encrypted mnemonic
+  if let Ok(mnemonic) = mnemonic {
+    return HttpResponse::Ok().body(mnemonic)
+  }
+  
+  // else create and store the encrypted mnemonic
   exec_basic_db_write_endpoint(
     store,
     Box::new(move || {
--- src/data/src/lib.rs
+++ src/data/src/lib.rs
@@ -2,3 +2,4 @@ pub mod repositories;
 pub mod helpers;
 pub mod types;
 pub mod serialization;
+pub mod models;
