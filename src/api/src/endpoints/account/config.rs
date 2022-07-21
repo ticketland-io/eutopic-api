d4cd367e37ce78baa9ab449c3bde823251c4cdaa
ppoliani <ppoliani@gmail.com>
2022-07-21 18:26:10 +0300

    refactor(api): remove comments

--- src/api/src/endpoints/account/config.rs
+++ src/api/src/endpoints/account/config.rs
@@ -8,10 +8,5 @@ pub fn config(cfg: &mut web::ServiceConfig) {
   cfg.service(
     web::resource("")
     .route(web::post().to(create_account::exec))
-    // .route(
-    //   web::get()
-    //     .to(get_check_in_users)
-    //     .guard(AuthzGuard::new(vec![Scope::AdminAll]))
-    // )
   );
 }
